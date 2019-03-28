5f5560c6dd00375bc01c23569bda7a1379cc0a49
root <a_joragumar@iestriana.com>
2019-03-28 09:50:08 +0100

    neptune V2

---
+++ exercise-04.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $( "#header-width" ).html($("h1").width());
+});
+
+$(document).ready(function(){
+    $( "#main-section-width" ).html($("div.section").height());
+});
